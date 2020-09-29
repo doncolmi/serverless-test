8c76062e3cb9f2af2f737f01277d0a323188ca54
doncolmi <matathresh1740@naver.com>
2020-09-29 11:59:06 +0900

    fix handler.js

--- crawling/handler.js
+++ crawling/handler.js
@@ -3,45 +3,38 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 module.exports.hello = async (event) => {
+  const app_key = "04e720df504f317519965123d03df7d7";
+  const config = {
+    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    Authorization: `KakaoAK ${app_key}`,
+  };
+  const translate = async (text) => {
+    const { data } = await axios({
+      method: "GET",
+      headers: config,
+      url: `https://dapi.kakao.com/v2/translation/translate?src_lang=en&target_lang=kr&query=${text}`,
+    });
+    return data.translated_text[0][0];
+  };
+
   const { data } = await axios.get(
     "https://www.bbc.com/sport/football/premier-league"
   );
   const $ = cheerio.load(data);
   const list = $("div.sp-qa-top-stories")
     .children("div.gel-layout__item")
-    .find("h3");
-  const listArray = list.toArray().map((element) => $(element).text());
+    .find("a");
+  const listArray = await list.toArray().map(async (element) => ({
+    text: $(element).find("h3").text(),
+    href: `https://www.bbc.com${$(element).attr("href")}`,
+    translated_text: await translate($(element).find("h3").text()),
+  }));
 
-  const config = {
-    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-    "X-Naver-Client-Id": "GxtkDmY2t5Hra3AV3iYW",
-    "X-Naver-Client-Secret": "33f3fAAojj",
-  };
-
-  for (const item of listArray) {
-    console.log(item);
-  }
-
-  let i = 0;
-  const TransTextArray = await listArray.map(async (element) => {
-    console.log(`번역중! ${++i}/10`);
-    const { data } = await axios({
-      method: "POST",
-      headers: config,
-      url: `https://openapi.naver.com/v1/papago/n2mt?source=en&target=ko&text=${element}`,
-    }).catch((e) => console.log(e));
-    console.log(data);
-    return data;
-  });
-
-  for (const item of TransTextArray) {
-    console.log(item);
-  }
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v1.0! Your function executed successfully!",
+        message: `${listArray[0]}, ${listArray[0].href}, ${listArray[0].translated_text}`,
         input: event,
       },
       null,
