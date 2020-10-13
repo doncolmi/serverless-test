8566d4b6f9f1ef32bc2078d0fdc60628361d0329
doncolmi <matathresh1740@naver.com>
2020-10-14 03:49:01 +0900

    fix s3.js

--- src/s3.js
+++ src/s3.js
@@ -1,5 +1,6 @@
 "use strict";
 const AWS = require("aws-sdk");
+const { aws, awsSecret } = require("./config/env.json")["development"];
 const fs = require("fs");
 const axios = require("axios");
 const image = require("imagemin");
@@ -7,26 +8,34 @@ const webp = require("imagemin-webp");
 
 module.exports.s3upload = async (event, context, callback) => {
   const s3 = new AWS.S3({
-    accessKeyId: "",
-    secretAccessKey: "",
+    accessKeyId: aws,
+    secretAccessKey: awsSecret,
     region: "ap-northeast-2",
   });
-  const url = [
-    "https://images.daznservices.com/di/library/GOAL/3c/d2/rodrygo-real-madrid-2019-20_1krmc78bpasvi1jiabf3i0m18b.jpg?",
-  ];
 
-  image(url, "images", {
-    use: [webp({ quality: 75 })],
-  }).then((result) => console.log(result));
+  const url =
+    "https://images.daznservices.com/di/library/GOAL/3c/d2/rodrygo-real-madrid-2019-20_1krmc78bpasvi1jiabf3i0m18b.jpg?";
 
-  // const s3Params = {
-  //     Bucket: 'thegreen-limc',
-  //     Key: "test.jpg",
-  //     ContentType: "image/jpeg",
-  //     ACL: 'public-read',
-  //     Body: data
-  // }
+  const { data } = await axios({
+    url,
+    responseType: "arraybuffer",
+  });
+  const hello = await image.buffer(data, {
+    plugins: [webp({ quality: 75 })],
+  });
 
-  // const hi = await s3.upload(s3Params, function (err, data) { console.log(err); console.log(data);})
-  // console.log(hi);
+  const s3Params = {
+    Bucket: "thegreen-limc",
+    Key: "test.webp",
+    ContentType: "image/webp",
+    ACL: "public-read",
+    Body: hello,
+  };
+
+  const hi = await s3.upload(s3Params).promise();
+  await callback(null, {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ wow: "hello" }),
+  });
 };
